a3023f9b346140f1116115e30bd735719f5b1f85
AnshuGupta00 <144334045+AnshuGupta00@users.noreply.github.com>
2023-09-07 16:17:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+// let a =prompt("what is your age?")
+
+// if(a>10){"hello"}
+// else(a<2)
+// alert("get lost")
+
+let a=prompt("whats your age?")
+  if(a<5){
+  alert("Get lost")
+  }
+else if (a<20)
+{
+alert("Most welcome")
+}
+else if(a>45){
+  alert("Rest in peace")
+}
+
+
